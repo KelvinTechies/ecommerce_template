bc17ec9a6877a1bd1e55c2986eefb8e74e28432c
kelvinTechies <iamkelvincole@gmail.com>
2024-03-20 15:03:52 +0100

    committing

---
+++ src/Subcomponents/Arrivals.jsx
@@ -0,0 +1,125 @@
+import { arrival_1 } from "../assets/Arrivals";
+
+function Arrivals() {
+  return (
+    <div className="mt-5 arrivals">
+      <div className="text-center">
+        <h2>New Arrivals</h2>
+      </div>
+      <div className="text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
+        tenetur distinctio nihil itaque.
+      </div>
+      <div className="row_1 mt-5 col-md-11">
+        <div className="pro  col-">
+          <div className="my_card">
+            <div className="my_row">
+              <span className="my_badge text-center  text-white">New</span>
+              <span className="discount text-center">-2%</span>
+            </div>
+            <img src={arrival_1} alt="" />
+          </div>
+          <div className="desc">
+            <span>Ariel Sneakers, xylum xperia</span>
+            <span>$124.00</span>
+          </div>
+        </div>
+        <div className="pro col-">
+          <div className="my_card">
+            <div className="my_row">
+              <span className="my_badge text-center  text-white">New</span>
+              <span className="discount text-center">-2%</span>
+            </div>
+            <img src={arrival_1} alt="" />
+          </div>
+          <div className="desc">
+            <span>Ariel Sneakers, xylum xperia</span>
+            <span>$124.00</span>
+          </div>
+        </div>
+        <div className="pro col-">
+          <div className="my_card">
+            <div className="my_row">
+              <span className="my_badge text-center  text-white">New</span>
+              <span className="discount text-center">-2%</span>
+            </div>
+            <img src={arrival_1} alt="" />
+          </div>
+          <div className="desc">
+            <span>Ariel Sneakers, xylum xperia</span>
+            <span>$124.00</span>
+          </div>
+        </div>
+        <div className="pro col-">
+          <div className="my_card">
+            <div className="my_row">
+              <span className="my_badge text-center  text-white">New</span>
+              <span className="discount text-center">-2%</span>
+            </div>
+            <img src={arrival_1} alt="" />
+          </div>
+          <div className="desc">
+            <span>Ariel Sneakers, xylum xperia</span>
+            <span>$124.00</span>
+          </div>
+        </div>
+      </div>
+      <div className="row_1 mt-5 col-md-11">
+        <div className="pro  col-">
+          <div className="my_card">
+            <div className="my_row">
+              <span className="my_badge text-center  text-white">New</span>
+              <span className="discount text-center">-2%</span>
+            </div>
+            <img src={arrival_1} alt="" />
+          </div>
+          <div className="desc">
+            <span>Ariel Sneakers, xylum xperia</span>
+            <span>$124.00</span>
+          </div>
+        </div>
+        <div className="pro col-">
+          <div className="my_card">
+            <div className="my_row">
+              <span className="my_badge text-center  text-white">New</span>
+              <span className="discount text-center">-2%</span>
+            </div>
+            <img src={arrival_1} alt="" />
+          </div>
+          <div className="desc">
+            <span>Ariel Sneakers, xylum xperia</span>
+            <span>$124.00</span>
+          </div>
+        </div>
+        <div className="pro col-">
+          <div className="my_card">
+            <div className="my_row">
+              <span className="my_badge text-center  text-white">New</span>
+              <span className="discount text-center">-2%</span>
+            </div>
+            <img src={arrival_1} alt="" />
+          </div>
+          <div className="desc">
+            <span>Ariel Sneakers, xylum xperia</span>
+            <span>$124.00</span>
+          </div>
+        </div>
+        <div className="pro col-">
+          <div className="my_card">
+            <div className="my_row">
+              <span className="my_badge text-center  text-white">New</span>
+              <span className="discount text-center">-2%</span>
+            </div>
+            <img src={arrival_1} alt="" />
+          </div>
+          <div className="desc">
+            <span>Ariel Sneakers, xylum xperia</span>
+            <span>$124.00</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Arrivals;
